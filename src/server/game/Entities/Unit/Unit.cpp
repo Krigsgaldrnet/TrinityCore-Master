Krigsgaldrnet/TrinityCore-Master

commit 3dff2d8fa7137bddca47c4696a92ab91be74a2b0

Core/Unit: Remove code which depends on polymorphism from Unit class destructor (at destruction time virtual methods aren't working as excpected)

src/server/game/Entities/Unit/Unit.cpp
@@ -204,11 +204,9 @@ Unit::~Unit()
         }
     }
 
-    RemoveAllGameObjects();
-    RemoveAllDynObjects();
     _DeleteRemovedAuras();
 
-    delete m_charmInfo;
+    ASSERT(!m_charmInfo);
     delete m_vehicleKit;
 
     ASSERT(!m_duringRemoveFromWorld);
@@ -219,6 +217,8 @@ Unit::~Unit()
     ASSERT(m_appliedAuras.empty());
     ASSERT(m_ownedAuras.empty());
     ASSERT(m_removedAuras.empty());
+    ASSERT(m_gameObj.empty());
+    ASSERT(m_dynObj.empty());
 }
 
 void Unit::Update(uint32 p_time)
@@ -13345,6 +13345,7 @@ void Unit::CleanupsBeforeDelete(bool finalCleanup)
     //A unit may be in removelist and not in world, but it is still in grid
     //and may have some references during delete
     RemoveAllAuras();
+    RemoveAllGameObjects();
 
     if (finalCleanup)
         m_cleanupDone = true;
@@ -13359,6 +13360,8 @@ void Unit::CleanupsBeforeDelete(bool finalCleanup)
     if (Creature* thisCreature = ToCreature())
         if (GetTransport())
             GetTransport()->RemovePassenger(thisCreature);
+
+    DeleteCharmInfo();
 }
 
 void Unit::UpdateCharmAI()
@@ -13417,7 +13420,6 @@ CharmInfo::CharmInfo(Unit* unit)
         m_oldReactState = m_unit->ToCreature()->GetReactState();
         m_unit->ToCreature()->SetReactState(REACT_PASSIVE);
     }
-
 }
 
 CharmInfo::~CharmInfo()

commit 110e9cecece22ec1e42a530c947e6580bd6b289d

Core/Unit: Move code using polymorphism out of CharmInfo destructor - fixes issues introduced with 3dff2d8fa7137bddca47.

src/server/game/Entities/Unit/Unit.cpp
@@ -206,7 +206,7 @@ Unit::~Unit()
 
     _DeleteRemovedAuras();
 
-    ASSERT(!m_charmInfo);
+    delete m_charmInfo;
     delete m_vehicleKit;
 
     ASSERT(!m_duringRemoveFromWorld);
@@ -13360,8 +13360,6 @@ void Unit::CleanupsBeforeDelete(bool finalCleanup)
     if (Creature* thisCreature = ToCreature())
         if (GetTransport())
             GetTransport()->RemovePassenger(thisCreature);
-
-    DeleteCharmInfo();
 }
 
 void Unit::UpdateCharmAI()
@@ -13404,6 +13402,7 @@ void Unit::DeleteCharmInfo()
     if (!m_charmInfo)
         return;
 
+    m_charmInfo->RestoreState();
     delete m_charmInfo;
     m_charmInfo = NULL;
 }
@@ -13423,6 +13422,10 @@ CharmInfo::CharmInfo(Unit* unit)
 }
 
 CharmInfo::~CharmInfo()
+{
+}
+
+void CharmInfo::RestoreState()
 {
     if (m_unit->GetTypeId() == TYPEID_UNIT)
         if (Creature *pCreature = m_unit->ToCreature())

src/server/game/Entities/Unit/Unit.h
@@ -1027,6 +1027,7 @@ struct CharmInfo
     public:
         explicit CharmInfo(Unit* unit);
         ~CharmInfo();
+        void RestoreState();
         uint32 GetPetNumber() const { return m_petnumber; }
         void SetPetNumber(uint32 petnumber, bool statwindow);
 
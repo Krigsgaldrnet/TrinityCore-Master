Krigsgaldrnet/TrinityCore-Master

commit 6323964f67af4d731513edc3b3f8ee5c10999915

Core/Channels: Fix the console error "opcode CMSG_MESSAGECHAT have unprocessed tail data" and some logic problems when joining/leaving some channels (like only be able to join LFG inside a city)

src/server/game/Server/Protocol/Handlers/ChannelHandler.cpp
@@ -26,20 +26,14 @@ void WorldSession::HandleJoinChannel(WorldPacket& recvPacket)
     uint32 channel_id;
     uint8 unknown1, unknown2;
     std::string channelname, pass;
+    ChatChannelsEntry const* channel;
 
     recvPacket >> channel_id;
 
     if (channel_id)
     {
-        ChatChannelsEntry const* channel = sChatChannelsStore.LookupEntry(channel_id);
+        channel = sChatChannelsStore.LookupEntry(channel_id);
         if (!channel)
-            return;
-
-        AreaTableEntry const* current_zone = GetAreaEntryByAreaID(_player->GetZoneId());
-        if (!current_zone)
-            return;
-
-        if (!_player->CanJoinConstantChannelInZone(channel, current_zone))
             return;
     }
 
@@ -50,6 +44,14 @@ void WorldSession::HandleJoinChannel(WorldPacket& recvPacket)
         return;
 
     recvPacket >> pass;
+
+    AreaTableEntry const* current_zone = GetAreaEntryByAreaID(_player->GetZoneId());
+    if (!current_zone)
+        return;
+
+    if (!_player->CanJoinConstantChannelInZone(channel, current_zone))
+        return;
+
     if (ChannelMgr* cMgr = channelMgr(_player->GetTeam()))
     {
         cMgr->team = _player->GetTeam();

src/server/game/Chat/Channels/Channel.h
@@ -95,12 +95,13 @@ enum ChannelDBCFlags
     CHANNEL_DBC_FLAG_INITIAL    = 0x00001,              // General, Trade, LocalDefense, LFG
     CHANNEL_DBC_FLAG_ZONE_DEP   = 0x00002,              // General, Trade, LocalDefense, GuildRecruitment
     CHANNEL_DBC_FLAG_GLOBAL     = 0x00004,              // WorldDefense
-    CHANNEL_DBC_FLAG_TRADE      = 0x00008,              // Trade
-    CHANNEL_DBC_FLAG_CITY_ONLY  = 0x00010,              // Trade, GuildRecruitment
-    CHANNEL_DBC_FLAG_CITY_ONLY2 = 0x00020,              // Trade, GuildRecruitment
+    CHANNEL_DBC_FLAG_TRADE      = 0x00008,              // Trade, LFG
+    CHANNEL_DBC_FLAG_CITY_ONLY  = 0x00010,              // Trade, GuildRecruitment, LFG
+    CHANNEL_DBC_FLAG_CITY_ONLY2 = 0x00020,              // Trade, GuildRecruitment, LFG
     CHANNEL_DBC_FLAG_DEFENSE    = 0x10000,              // LocalDefense, WorldDefense
     CHANNEL_DBC_FLAG_GUILD_REQ  = 0x20000,              // GuildRecruitment
-    CHANNEL_DBC_FLAG_LFG        = 0x40000               // LookingForGroup
+    CHANNEL_DBC_FLAG_LFG        = 0x40000,              // LFG
+    CHANNEL_DBC_FLAG_UNK1       = 0x80000,              // General
 };
 
 enum ChannelMemberFlags

src/server/game/Entities/Player/Player.cpp
@@ -5522,14 +5522,14 @@ void Player::RepopAtGraveyard()
 
 bool Player::CanJoinConstantChannelInZone(ChatChannelsEntry const* channel, AreaTableEntry const* zone)
 {
-    if (channel->flags & CHANNEL_DBC_FLAG_ZONE_DEP)
-    {
-        if (zone->flags & AREA_FLAG_ARENA_INSTANCE)
-            return false;
+    if (channel->flags & CHANNEL_DBC_FLAG_ZONE_DEP && zone->flags & AREA_FLAG_ARENA_INSTANCE)
+        return false;
 
-        if ((channel->flags & CHANNEL_DBC_FLAG_CITY_ONLY) && !(zone->flags & AREA_FLAG_CAPITAL))
-            return false;
-    }
+    if ((channel->flags & CHANNEL_DBC_FLAG_CITY_ONLY) && (!(zone->flags & AREA_FLAG_SLAVE_CAPITAL)))
+        return false;
+
+    if ((channel->flags & CHANNEL_DBC_FLAG_GUILD_REQ) && GetGuildId())
+        return false;
 
     return true;
 }
@@ -5577,12 +5577,6 @@ void Player::UpdateLocalChannels(uint32 newZone)
     {
         if (ChatChannelsEntry const* channel = sChatChannelsStore.LookupEntry(i))
         {
-            if (!(channel->flags & CHANNEL_DBC_FLAG_ZONE_DEP))
-                continue;                                    // Not zone dependent, don't handle it here
-
-            if ((channel->flags & CHANNEL_DBC_FLAG_GUILD_REQ) && GetGuildId())
-                continue;                                    // Should not join to these channels automatically
-
             Channel* usedChannel = NULL;
 
             for (JoinedChannelsList::iterator itr = m_channels.begin(); itr != m_channels.end(); ++itr)

commit 1dba7ac597c811dd6035e17c3af1d094625b6402

Core/Channels: Fix crash in HandleJoinChannel introduced with commit 6323964f67af4d731513.

src/server/game/Server/Protocol/Handlers/ChannelHandler.cpp
@@ -26,14 +26,20 @@ void WorldSession::HandleJoinChannel(WorldPacket& recvPacket)
     uint32 channel_id;
     uint8 unknown1, unknown2;
     std::string channelname, pass;
-    ChatChannelsEntry const* channel;
 
     recvPacket >> channel_id;
 
     if (channel_id)
     {
-        channel = sChatChannelsStore.LookupEntry(channel_id);
+        ChatChannelsEntry const* channel = sChatChannelsStore.LookupEntry(channel_id);
         if (!channel)
+            return;
+
+        AreaTableEntry const* current_zone = GetAreaEntryByAreaID(_player->GetZoneId());
+        if (!current_zone)
+            return;
+
+        if (!_player->CanJoinConstantChannelInZone(channel, current_zone))
             return;
     }
 
@@ -45,17 +51,10 @@ void WorldSession::HandleJoinChannel(WorldPacket& recvPacket)
 
     recvPacket >> pass;
 
-    AreaTableEntry const* current_zone = GetAreaEntryByAreaID(_player->GetZoneId());
-    if (!current_zone)
-        return;
-
-    if (!_player->CanJoinConstantChannelInZone(channel, current_zone))
-        return;
-
     if (ChannelMgr* cMgr = channelMgr(_player->GetTeam()))
     {
         cMgr->team = _player->GetTeam();
-        if (Channel *chn = cMgr->GetJoinChannel(channelname, channel_id))
+        if (Channel* chn = cMgr->GetJoinChannel(channelname, channel_id))
             chn->Join(_player->GetGUID(), pass.c_str());
     }
 }
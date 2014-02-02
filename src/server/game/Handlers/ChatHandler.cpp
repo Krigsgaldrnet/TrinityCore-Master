Krigsgaldrnet/TrinityCore-Master

commit 1a006b36d2ae1853b85df5dd60c81fcaf034b05e

Core/Chat: Fix an exploit where it was possible to speak in universal language in say/yell/emote by sending CMSG_MESSAGECHAT using cheats.

src/server/game/Handlers/ChatHandler.cpp
@@ -55,6 +55,14 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recvData)
         return;
     }
 
+    if (lang == LANG_UNIVERSAL)
+    {
+        TC_LOG_ERROR("network", "CMSG_MESSAGECHAT: Possible hacking-attempt: %s tried to send a message in universal language", GetPlayerInfo().c_str());
+        SendNotification(LANG_UNKNOWN_LANGUAGE);
+        recvData.rfinish();
+        return;
+    }
+
     Player* sender = GetPlayer();
 
     //TC_LOG_DEBUG("CHAT: packet received. type %u, lang %u", type, lang);

commit 6d2eb49997b2dbb76f63c739b0b6066caad41be9

Core/Chat: Fix a new issue caused by 1a006b36d2ae1853b85df5dd60c81fcaf034b05e which would trigger false positives for CHAT_MSG_AFK and CHAT_MSG_DND.

src/server/game/Handlers/ChatHandler.cpp
@@ -55,7 +55,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recvData)
         return;
     }
 
-    if (lang == LANG_UNIVERSAL)
+    if (lang == LANG_UNIVERSAL && type != CHAT_MSG_AFK && type != CHAT_MSG_DND)
     {
         TC_LOG_ERROR("network", "CMSG_MESSAGECHAT: Possible hacking-attempt: %s tried to send a message in universal language", GetPlayerInfo().c_str());
         SendNotification(LANG_UNKNOWN_LANGUAGE);
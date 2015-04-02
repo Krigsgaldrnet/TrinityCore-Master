Krigsgaldrnet/TrinityCore-Master

commit d0fc28b2e9b5cd3e98c23f121dea1251a5b5a634

Core/Unit: Implement SMSG_HEALTH_UPDATE

src/server/game/Entities/Unit/Unit.cpp
@@ -10094,6 +10094,19 @@ int32 Unit::ModifyHealth(int32 dVal)
         gain = maxHealth - curHealth;
     }
 
+    if (dVal < 0)
+    {
+        WorldPackets::Combat::HealthUpdate packet;
+        packet.Guid = GetGUID();
+        packet.Health = GetHealth();
+        
+        if (GetTypeId() == TYPEID_PLAYER)
+            ToPlayer()->GetSession()->SendPacket(packet.Write());
+        else
+            if (Player* player = GetOwner()->ToPlayer())
+                player->GetSession()->SendPacket(packet.Write());
+    }
+
     return gain;
 }
 

src/server/game/Server/Protocol/Opcodes.cpp
@@ -1210,7 +1210,7 @@ void OpcodeTable::Initialize()
     DEFINE_SERVER_OPCODE_HANDLER(SMSG_GUILD_ROSTER,                            STATUS_UNHANDLED,    CONNECTION_TYPE_REALM);
     DEFINE_SERVER_OPCODE_HANDLER(SMSG_GUILD_ROSTER_UPDATE,                     STATUS_UNHANDLED,    CONNECTION_TYPE_REALM);
     DEFINE_SERVER_OPCODE_HANDLER(SMSG_GUILD_SEND_RANK_CHANGE,                  STATUS_UNHANDLED,    CONNECTION_TYPE_REALM);
-    DEFINE_SERVER_OPCODE_HANDLER(SMSG_HEALTH_UPDATE,                           STATUS_UNHANDLED,    CONNECTION_TYPE_REALM);
+    DEFINE_SERVER_OPCODE_HANDLER(SMSG_HEALTH_UPDATE,                           STATUS_NEVER,        CONNECTION_TYPE_REALM);
     DEFINE_SERVER_OPCODE_HANDLER(SMSG_HIGHEST_THREAT_UPDATE,                   STATUS_NEVER,        CONNECTION_TYPE_INSTANCE);
     DEFINE_SERVER_OPCODE_HANDLER(SMSG_HOTFIX_NOTIFY,                           STATUS_UNHANDLED,    CONNECTION_TYPE_REALM);
     DEFINE_SERVER_OPCODE_HANDLER(SMSG_HOTFIX_NOTIFY_BLOB,                      STATUS_NEVER,        CONNECTION_TYPE_REALM);

commit 7f3da3f755fe95fc930a4e3361761e25db8200e0

Core/Unit: Fix crash introduced by d0fc28b2e9b5cd3e98c23f121dea1251a5b5a634

src/server/game/Entities/Unit/Unit.cpp
@@ -10100,11 +10100,9 @@ int32 Unit::ModifyHealth(int32 dVal)
         packet.Guid = GetGUID();
         packet.Health = GetHealth();
         
-        if (GetTypeId() == TYPEID_PLAYER)
-            ToPlayer()->GetSession()->SendPacket(packet.Write());
-        else
-            if (Player* player = GetOwner()->ToPlayer())
-                player->GetSession()->SendPacket(packet.Write());
+        if (Player* player = GetCharmerOrOwnerPlayerOrPlayerItself())
+            player->GetSession()->SendPacket(packet.Write());
+
     }
 
     return gain;
Krigsgaldrnet/TrinityCore-Master

commit 5e85c75e3ae0e8a5a9e9da63dcdca2d4de006767

Core/Updater: Add some info about the count of applied updates. * some minor improvements * add a log message when database updates are disabled.
(cherry picked from commit a5b3862bc1b066a9614369bda3adef485a57eff8)

src/server/shared/Updater/DBUpdater.cpp
@@ -271,10 +271,10 @@ bool DBUpdater<T>::Update(DatabaseWorkerPool<T>& pool)
         [&](Path const& file) { DBUpdater<T>::ApplyFile(pool, file); },
             [&](std::string const& query) -> QueryResult { return DBUpdater<T>::Retrieve(pool, query); });
 
-    uint32 count;
+    UpdateResult result;
     try
     {
-        count = updateFetcher.Update(
+        result = updateFetcher.Update(
             sConfigMgr->GetBoolDefault("Updates.Redundancy", true),
             sConfigMgr->GetBoolDefault("Updates.AllowRehash", true),
             sConfigMgr->GetBoolDefault("Updates.ArchivedRedundancy", false),
@@ -285,10 +285,13 @@ bool DBUpdater<T>::Update(DatabaseWorkerPool<T>& pool)
         return false;
     }
 
-    if (!count)
-        TC_LOG_INFO("sql.updates", ">> %s database is up-to-date!", DBUpdater<T>::GetTableName().c_str());
+    std::string const info = Trinity::StringFormat("Containing " SZFMTD " new and " SZFMTD " archived updates.",
+        result.recent, result.archived);
+
+    if (!result.updated)
+        TC_LOG_INFO("sql.updates", ">> %s database is up-to-date! %s", DBUpdater<T>::GetTableName().c_str(), info.c_str());
     else
-        TC_LOG_INFO("sql.updates", ">> Applied %d %s.", count, count == 1 ? "query" : "queries");
+        TC_LOG_INFO("sql.updates", ">> Applied %d %s. %s", result.updated, result.updated == 1 ? "query" : "queries", info.c_str());
 
     return true;
 }

src/server/shared/Database/DatabaseLoader.cpp
@@ -117,6 +117,9 @@ DatabaseLoader& DatabaseLoader::AddDatabase(DatabaseWorkerPool<T>& pool, std::st
 
 bool DatabaseLoader::Load()
 {
+    if (!_updateFlags)
+        TC_LOG_INFO("sql.updates", "Automatic database updates are disabled for all databases!");
+
     if (!OpenDatabases())
         return false;
 

src/server/shared/Updater/DBUpdater.h
@@ -41,6 +41,19 @@ enum BaseLocation
     LOCATION_DOWNLOAD
 };
 
+struct UpdateResult
+{
+    UpdateResult()
+        : updated(0), recent(0), archived(0) { }
+
+    UpdateResult(size_t const updated_, size_t const recent_, size_t const archived_)
+        : updated(updated_), recent(recent_), archived(archived_) { }
+
+    size_t updated;
+    size_t recent;
+    size_t archived;
+};
+
 template <class T>
 class DBUpdater
 {

src/server/shared/Updater/UpdateFetcher.cpp
@@ -154,17 +154,27 @@ UpdateFetcher::SQLUpdate UpdateFetcher::ReadSQLUpdate(boost::filesystem::path co
     return update;
 }
 
-uint32 UpdateFetcher::Update(bool const redundancyChecks, bool const allowRehash, bool const archivedRedundancy, int32 const cleanDeadReferencesMaxCount) const
+UpdateResult UpdateFetcher::Update(bool const redundancyChecks, bool const allowRehash, bool const archivedRedundancy, int32 const cleanDeadReferencesMaxCount) const
 {
     LocaleFileStorage const available = GetFileList();
     AppliedFileStorage applied = ReceiveAppliedFiles();
+
+    size_t countRecentUpdates = 0;
+    size_t countArchivedUpdates = 0;
+
+    // Count updates
+    for (auto const& entry : applied)
+        if (entry.second.state == RELEASED)
+            ++countRecentUpdates;
+        else
+            ++countArchivedUpdates;
 
     // Fill hash to name cache
     HashToFileNameStorage hashToName;
     for (auto entry : applied)
         hashToName.insert(std::make_pair(entry.second.hash, entry.first));
 
-    uint32 importedUpdates = 0;
+    size_t importedUpdates = 0;
 
     for (auto const& availableQuery : available)
     {
@@ -314,7 +324,7 @@ uint32 UpdateFetcher::Update(bool const redundancyChecks, bool const allowRehash
         }
     }
 
-    return importedUpdates;
+    return UpdateResult(importedUpdates, countRecentUpdates, countArchivedUpdates);
 }
 
 std::string UpdateFetcher::CalculateHash(SQLUpdate const& query) const
@@ -329,7 +339,6 @@ std::string UpdateFetcher::CalculateHash(SQLUpdate const& query) const
 uint32 UpdateFetcher::Apply(Path const& path) const
 {
     using Time = std::chrono::high_resolution_clock;
-    using ms = std::chrono::milliseconds;
 
     // Benchmark query speed
     auto const begin = Time::now();
@@ -338,7 +347,7 @@ uint32 UpdateFetcher::Apply(Path const& path) const
     _applyFile(path);
 
     // Return time the query took to apply
-    return std::chrono::duration_cast<ms>(Time::now() - begin).count();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(Time::now() - begin).count();
 }
 
 void UpdateFetcher::UpdateEntry(AppliedFileEntry const& entry, uint32 const speed) const

src/server/shared/Updater/UpdateFetcher.h
@@ -35,7 +35,7 @@ public:
         std::function<void(Path const& path)> const& applyFile,
         std::function<QueryResult(std::string const&)> const& retrieve);
 
-    uint32 Update(bool const redundancyChecks, bool const allowRehash,
+    UpdateResult Update(bool const redundancyChecks, bool const allowRehash,
                   bool const archivedRedundancy, int32 const cleanDeadReferencesMaxCount) const;
 
 private:

commit 95754772d1215af7f03b5e73f11c2ea93a37270b

Core/Updater: Fix a wrong formatter introduced in 5e85c75e3ae0e * thanks @Aokromes for noticing
(cherry picked from commit bd661b58fd2c37850a719cfffe135e89043c112d)

src/server/shared/Updater/DBUpdater.cpp
@@ -291,7 +291,7 @@ bool DBUpdater<T>::Update(DatabaseWorkerPool<T>& pool)
     if (!result.updated)
         TC_LOG_INFO("sql.updates", ">> %s database is up-to-date! %s", DBUpdater<T>::GetTableName().c_str(), info.c_str());
     else
-        TC_LOG_INFO("sql.updates", ">> Applied %d %s. %s", result.updated, result.updated == 1 ? "query" : "queries", info.c_str());
+        TC_LOG_INFO("sql.updates", ">> Applied " SZFMTD " %s. %s", result.updated, result.updated == 1 ? "query" : "queries", info.c_str());
 
     return true;
 }
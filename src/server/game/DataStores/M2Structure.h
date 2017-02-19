bachda90/TrinityCore

[Core/Maps] Activate creatures and objects during opening cinematics and other flyby cameras (sunwell etc).
Requires re-running map extractor to extract camera m2 files from data
files. These are very small.

src/server/game/DataStores/M2Structure.h
@@ -1,0 +1,137 @@
+/*
+ * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TRINITY_M2STRUCTURE_H
+#define TRINITY_M2STRUCTURE_H
+
+#include "Common.h"
+#include "Util.h"
+
+#include <G3D/Vector3.h>
+#include <G3D/AABox.h>
+
+// Structures using to access raw DBC data and required packing to portability
+#pragma pack(push, 1)
+// Structures for M2 file. Source: https://wowdev.wiki
+template<typename T>
+struct M2SplineKey
+{
+    T p0;
+    T p1;
+    T p2;
+};
+
+struct M2Header
+{
+    char   Magic[4];               // "MD21"
+    uint32 Version;                // The version of the format.
+    uint32 lName;                  // Length of the model's name including the trailing \0
+    uint32 ofsName;                // Offset to the name, it seems like models can get reloaded by this name.should be unique, i guess.
+    uint32 GlobalModelFlags;       // 0x0001: tilt x, 0x0002: tilt y, 0x0008: add 2 fields in header, 0x0020: load .phys data (MoP+), 0x0080: has _lod .skin files (MoP?+), 0x0100: is camera related.
+    uint32 nGlobalSequences;
+    uint32 ofsGlobalSequences;     // A list of timestamps.
+    uint32 nAnimations;
+    uint32 ofsAnimations;          // Information about the animations in the model.
+    uint32 nAnimationLookup;
+    uint32 ofsAnimationLookup;     // Mapping of global IDs to the entries in the Animation sequences block.
+    uint32 nBones;                 // MAX_BONES = 0x100
+    uint32 ofsBones;               // Information about the bones in this model.
+    uint32 nKeyBoneLookup;
+    uint32 ofsKeyBoneLookup;       // Lookup table for key skeletal bones.
+    uint32 nVertices;
+    uint32 ofsVertices;            // Vertices of the model.
+    uint32 nViews;                 // Views (LOD) are now in .skins.
+    uint32 nSubmeshAnimations;
+    uint32 ofsSubmeshAnimations;   // Submesh color and alpha animations definitions.
+    uint32 nTextures;
+    uint32 ofsTextures;            // Textures of this model.
+    uint32 nTransparency;
+    uint32 ofsTransparency;        // Transparency of textures.
+    uint32 nUVAnimation;
+    uint32 ofsUVAnimation;
+    uint32 nTexReplace;
+    uint32 ofsTexReplace;          // Replaceable Textures.
+    uint32 nRenderFlags;
+    uint32 ofsRenderFlags;         // Blending modes / render flags.
+    uint32 nBoneLookupTable;
+    uint32 ofsBoneLookupTable;     // A bone lookup table.
+    uint32 nTexLookup;
+    uint32 ofsTexLookup;           // The same for textures.
+    uint32 nTexUnits;              // possibly removed with cata?!
+    uint32 ofsTexUnits;            // And texture units. Somewhere they have to be too.
+    uint32 nTransLookup;
+    uint32 ofsTransLookup;         // Everything needs its lookup. Here are the transparencies.
+    uint32 nUVAnimLookup;
+    uint32 ofsUVAnimLookup;
+    G3D::AABox BoundingBox;            // min/max( [1].z, 2.0277779f ) - 0.16f seems to be the maximum camera height
+    float  BoundingSphereRadius;
+    G3D::AABox CollisionBox;
+    float  CollisionSphereRadius;
+    uint32 nBoundingTriangles;
+    uint32 ofsBoundingTriangles;   // Our bounding volumes. Similar structure like in the old ofsViews.
+    uint32 nBoundingVertices;
+    uint32 ofsBoundingVertices;
+    uint32 nBoundingNormals;
+    uint32 ofsBoundingNormals;
+    uint32 nAttachments;
+    uint32 ofsAttachments;         // Attachments are for weapons etc.
+    uint32 nAttachLookup;
+    uint32 ofsAttachLookup;        // Of course with a lookup.
+    uint32 nEvents;
+    uint32 ofsEvents;              // Used for playing sounds when dying and a lot else.
+    uint32 nLights;
+    uint32 ofsLights;              // Lights are mainly used in loginscreens but in wands and some doodads too.
+    uint32 nCameras;               // Format of Cameras changed with version 271!
+    uint32 ofsCameras;             // The cameras are present in most models for having a model in the Character-Tab.
+    uint32 nCameraLookup;
+    uint32 ofsCameraLookup;        // And lookup-time again.
+    uint32 nRibbonEmitters;
+    uint32 ofsRibbonEmitters;      // Things swirling around. See the CoT-entrance for light-trails.
+    uint32 nParticleEmitters;
+    uint32 ofsParticleEmitters;    // Spells and weapons, doodads and loginscreens use them. Blood dripping of a blade? Particles.
+    uint32 nBlendMaps;             // This has to deal with blending. Exists IFF (flags & 0x8) != 0. When set, textures blending is overriden by the associated array. See M2/WotLK#Blend_mode_overrides
+    uint32 ofsBlendMaps;           // Same as above. Points to an array of uint16 of nBlendMaps entries -- From WoD information.};
+};
+
+struct M2Array
+{
+    uint32_t number;
+    uint32 offset_elements;
+};
+struct M2Track
+{
+    uint16_t interpolation_type;
+    uint16_t global_sequence;
+    M2Array timestamps;
+    M2Array values;
+};
+
+struct M2Camera
+{
+    uint32_t type; // 0: portrait, 1: characterinfo; -1: else (flyby etc.); referenced backwards in the lookup table.
+    float far_clip;
+    float near_clip;
+    M2Track positions; // How the camera's position moves. Should be 3*3 floats.
+    G3D::Vector3 position_base;
+    M2Track target_positions; // How the target moves. Should be 3*3 floats.
+    G3D::Vector3 target_position_base;
+    M2Track rolldata; // The camera can have some roll-effect. Its 0 to 2*Pi.
+    M2Track fovdata;  // FoV for this segment
+};
+#pragma pack(pop)
+
+#endif

src/tools/map_extractor/System.cpp
@@ -78,8 +78,20 @@ typedef struct
 
 std::vector<map_id> map_ids;
 std::vector<uint16> LiqType;
+std::vector<std::string> CameraFileNames;
 boost::filesystem::path input_path;
 boost::filesystem::path output_path;
+
+struct CinematicCameraMeta
+{
+    static DB2Meta const* Instance()
+    {
+        static char const* types = "sffh";
+        static uint8 const arraySizes[4] = { 1, 3, 1, 1 };
+        static DB2Meta instance(-1, 4, 0xA7B95349, types, arraySizes);
+        return &instance;
+    }
+};
 
 struct LiquidTypeMeta
 {
@@ -110,11 +122,12 @@ enum Extract
 {
     EXTRACT_MAP = 1,
     EXTRACT_DBC = 2,
+    EXTRACT_CAMERA = 4,
     EXTRACT_GT  = 8
 };
 
 // Select data for extract
-int   CONF_extract = EXTRACT_MAP | EXTRACT_DBC | EXTRACT_GT;
+int   CONF_extract = EXTRACT_MAP | EXTRACT_DBC | EXTRACT_CAMERA | EXTRACT_GT;
 
 // This option allow limit minimum height to some value (Allow save some memory)
 bool  CONF_allow_height_limit = true;
@@ -177,7 +190,7 @@ void Usage(char const* prg)
         "%s -[var] [value]\n"\
         "-i set input path\n"\
         "-o set output path\n"\
-        "-e extract only MAP(1)/DBC(2)/gt(8) - standard: all(11)\n"\
+        "-e extract only MAP(1)/DBC(2)/Camera(4)/gt(8) - standard: all(11)\n"\
         "-f height stored as int (less map size but lost some accuracy) 1 by default\n"\
         "-l dbc locale\n"\
         "Example: %s -f 0 -i \"c:\\games\\game\"\n", prg, prg);
@@ -190,10 +203,10 @@ void HandleArgs(int argc, char* arg[])
     {
         // i - input path
         // o - output path
-        // e - extract only MAP(1)/DBC(2) - standard both(3)
+        // e - extract only MAP(1)/DBC(2)/Camera(4)/gt(8) - standard: all(11)
         // f - use float to int conversion
         // h - limit minimum height
-        // b - target client build
+        // l - dbc locale
         if (arg[c][0] != '-')
             Usage(arg[0]);
 
@@ -294,7 +307,7 @@ uint32 ReadBuild(int locale)
 
 void ReadMapDBC()
 {
-    printf("Read Map.dbc file... ");
+    printf("Read Map.db2 file...\n");
 
     CASC::FileHandle dbcFile = CASC::OpenFile(CascStorage, "DBFilesClient\\Map.db2", CASC_LOCALE_NONE, true);
     if (!dbcFile)
@@ -350,7 +363,7 @@ void ReadMapDBC()
 
 void ReadLiquidTypeTableDBC()
 {
-    printf("Read LiquidType.dbc file...");
+    printf("Read LiquidType.db2 file...\n");
     CASC::FileHandle dbcFile = CASC::OpenFile(CascStorage, "DBFilesClient\\LiquidType.db2", CASC_LOCALE_NONE, true);
     if (!dbcFile)
     {
@@ -381,6 +394,38 @@ void ReadLiquidTypeTableDBC()
         LiqType[db2.GetRowCopy(x).second] = LiqType[db2.GetRowCopy(x).first];
 
     printf("Done! (" SZFMTD " LiqTypes loaded)\n", LiqType.size());
+}
+
+bool ReadCinematicCameraDBC()
+{
+    printf("Read CinematicCamera.db2 file...\n");
+
+    CASC::FileHandle dbcFile = CASC::OpenFile(CascStorage, "DBFilesClient\\CinematicCamera.db2", CASC_LOCALE_NONE, true);
+    if (!dbcFile)
+    {
+        printf("Unable to open CinematicCamera.db2. Camera extract aborted.\n");
+        return false;
+    }
+
+    DB2FileLoader db2;
+    if (!db2.Load(dbcFile, CinematicCameraMeta::Instance()))
+    {
+        printf("Invalid CinematicCamera.db2 file format. Camera extract aborted. %s\n", HumanReadableCASCError(GetLastError()));
+        return false;
+    }
+
+    // get camera file list from DB2
+    for (size_t i = 0; i < db2.GetNumRows(); ++i)
+    {
+        std::string camFile(db2.getRecord(i).getString(0, 0));
+        size_t loc = camFile.find(".mdx");
+        if (loc != std::string::npos)
+            camFile.replace(loc, 4, ".m2");
+        CameraFileNames.push_back(camFile);
+    }
+
+    printf("Done! (" SZFMTD " CinematicCameras loaded)\n", CameraFileNames.size());
+    return true;
 }
 
 //
@@ -1192,6 +1237,38 @@ void ExtractDBFilesClient(int l)
     printf("Extracted %u files\n\n", count);
 }
 
+void ExtractCameraFiles()
+{
+    printf("Extracting camera files...\n");
+
+    if (!ReadCinematicCameraDBC())
+        return;
+
+    boost::filesystem::path outputPath = output_path / "cameras";
+
+    CreateDir(outputPath);
+
+    printf("output path %s\n", outputPath.string().c_str());
+
+    // extract M2s
+    uint32 count = 0;
+    for (std::string const& cameraFileName : CameraFileNames)
+    {
+        if (CASC::FileHandle dbcFile = CASC::OpenFile(CascStorage, cameraFileName.c_str(), CASC_LOCALE_NONE))
+        {
+            boost::filesystem::path filePath = outputPath / GetCascFilenamePart(cameraFileName.c_str());
+
+            if (!boost::filesystem::exists(filePath))
+                if (ExtractFile(dbcFile, filePath.string()))
+                    ++count;
+        }
+        else
+            printf("Unable to open file %s in the archive: %s\n", cameraFileName.c_str(), HumanReadableCASCError(GetLastError()));
+    }
+
+    printf("Extracted %u camera files\n", count);
+}
+
 void ExtractGameTables()
 {
     printf("Extracting game tables...\n");
@@ -1341,6 +1418,13 @@ int main(int argc, char * arg[])
     {
         printf("No locales detected\n");
         return 0;
+    }
+
+    if (CONF_extract & EXTRACT_CAMERA)
+    {
+        OpenCascStorage(FirstLocale);
+        ExtractCameraFiles();
+        CascStorage.reset();
     }
 
     if (CONF_extract & EXTRACT_GT)

Krigsgaldrnet/TrinityCore-Master

src/server/game/DataStores/M2Structure.h
@@ -1,0 +1,137 @@
+/*
+ * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TRINITY_M2STRUCTURE_H
+#define TRINITY_M2STRUCTURE_H
+
+#include "Common.h"
+#include "Util.h"
+
+#include <G3D/Vector3.h>
+#include <G3D/AABox.h>
+
+// Structures using to access raw DBC data and required packing to portability
+#pragma pack(push, 1)
+// Structures for M2 file. Source: https://wowdev.wiki
+template<typename T>
+struct M2SplineKey
+{
+    T p0;
+    T p1;
+    T p2;
+};
+
+struct M2Header
+{
+    char   Magic[4];               // "MD20"
+    uint32 Version;                // The version of the format.
+    uint32 lName;                  // Length of the model's name including the trailing \0
+    uint32 ofsName;                // Offset to the name, it seems like models can get reloaded by this name.should be unique, i guess.
+    uint32 GlobalModelFlags;       // 0x0001: tilt x, 0x0002: tilt y, 0x0008: add 2 fields in header, 0x0020: load .phys data (MoP+), 0x0080: has _lod .skin files (MoP?+), 0x0100: is camera related.
+    uint32 nGlobalSequences;
+    uint32 ofsGlobalSequences;     // A list of timestamps.
+    uint32 nAnimations;
+    uint32 ofsAnimations;          // Information about the animations in the model.
+    uint32 nAnimationLookup;
+    uint32 ofsAnimationLookup;     // Mapping of global IDs to the entries in the Animation sequences block.
+    uint32 nBones;                 // MAX_BONES = 0x100
+    uint32 ofsBones;               // Information about the bones in this model.
+    uint32 nKeyBoneLookup;
+    uint32 ofsKeyBoneLookup;       // Lookup table for key skeletal bones.
+    uint32 nVertices;
+    uint32 ofsVertices;            // Vertices of the model.
+    uint32 nViews;                 // Views (LOD) are now in .skins.
+    uint32 nSubmeshAnimations;
+    uint32 ofsSubmeshAnimations;   // Submesh color and alpha animations definitions.
+    uint32 nTextures;
+    uint32 ofsTextures;            // Textures of this model.
+    uint32 nTransparency;
+    uint32 ofsTransparency;        // Transparency of textures.
+    uint32 nUVAnimation;
+    uint32 ofsUVAnimation;
+    uint32 nTexReplace;
+    uint32 ofsTexReplace;          // Replaceable Textures.
+    uint32 nRenderFlags;
+    uint32 ofsRenderFlags;         // Blending modes / render flags.
+    uint32 nBoneLookupTable;
+    uint32 ofsBoneLookupTable;     // A bone lookup table.
+    uint32 nTexLookup;
+    uint32 ofsTexLookup;           // The same for textures.
+    uint32 nTexUnits;              // possibly removed with cata?!
+    uint32 ofsTexUnits;            // And texture units. Somewhere they have to be too.
+    uint32 nTransLookup;
+    uint32 ofsTransLookup;         // Everything needs its lookup. Here are the transparencies.
+    uint32 nUVAnimLookup;
+    uint32 ofsUVAnimLookup;
+    G3D::AABox BoundingBox;            // min/max( [1].z, 2.0277779f ) - 0.16f seems to be the maximum camera height
+    float  BoundingSphereRadius;
+    G3D::AABox CollisionBox;
+    float  CollisionSphereRadius;
+    uint32 nBoundingTriangles;
+    uint32 ofsBoundingTriangles;   // Our bounding volumes. Similar structure like in the old ofsViews.
+    uint32 nBoundingVertices;
+    uint32 ofsBoundingVertices;
+    uint32 nBoundingNormals;
+    uint32 ofsBoundingNormals;
+    uint32 nAttachments;
+    uint32 ofsAttachments;         // Attachments are for weapons etc.
+    uint32 nAttachLookup;
+    uint32 ofsAttachLookup;        // Of course with a lookup.
+    uint32 nEvents;
+    uint32 ofsEvents;              // Used for playing sounds when dying and a lot else.
+    uint32 nLights;
+    uint32 ofsLights;              // Lights are mainly used in loginscreens but in wands and some doodads too.
+    uint32 nCameras;               // Format of Cameras changed with version 271!
+    uint32 ofsCameras;             // The cameras are present in most models for having a model in the Character-Tab.
+    uint32 nCameraLookup;
+    uint32 ofsCameraLookup;        // And lookup-time again.
+    uint32 nRibbonEmitters;
+    uint32 ofsRibbonEmitters;      // Things swirling around. See the CoT-entrance for light-trails.
+    uint32 nParticleEmitters;
+    uint32 ofsParticleEmitters;    // Spells and weapons, doodads and loginscreens use them. Blood dripping of a blade? Particles.
+    uint32 nBlendMaps;             // This has to deal with blending. Exists IFF (flags & 0x8) != 0. When set, textures blending is overriden by the associated array. See M2/WotLK#Blend_mode_overrides
+    uint32 ofsBlendMaps;           // Same as above. Points to an array of uint16 of nBlendMaps entries -- From WoD information.};
+};
+
+struct M2Array
+{
+    uint32_t number;
+    uint32 offset_elements;
+};
+struct M2Track
+{
+    uint16_t interpolation_type;
+    uint16_t global_sequence;
+    M2Array timestamps;
+    M2Array values;
+};
+
+struct M2Camera
+{
+    uint32_t type; // 0: portrait, 1: characterinfo; -1: else (flyby etc.); referenced backwards in the lookup table.
+    float far_clip;
+    float near_clip;
+    M2Track positions; // How the camera's position moves. Should be 3*3 floats.
+    G3D::Vector3 position_base;
+    M2Track target_positions; // How the target moves. Should be 3*3 floats.
+    G3D::Vector3 target_position_base;
+    M2Track rolldata; // The camera can have some roll-effect. Its 0 to 2*Pi.
+    M2Track fovdata;  // FoV for this segment
+};
+#pragma pack(pop)
+
+#endif

src/tools/map_extractor/System.cpp
@@ -78,8 +78,20 @@ typedef struct
 
 std::vector<map_id> map_ids;
 std::vector<uint16> LiqType;
+std::set<std::string> CameraFileNames;
 boost::filesystem::path input_path;
 boost::filesystem::path output_path;
+
+struct CinematicCameraMeta
+{
+    static DB2Meta const* Instance()
+    {
+        static char const* types = "sffh";
+        static uint8 const arraySizes[4] = { 1, 3, 1, 1 };
+        static DB2Meta instance(-1, 4, 0xA7B95349, types, arraySizes);
+        return &instance;
+    }
+};
 
 struct LiquidTypeMeta
 {
@@ -110,11 +122,12 @@ enum Extract
 {
     EXTRACT_MAP = 1,
     EXTRACT_DBC = 2,
+    EXTRACT_CAMERA = 4,
     EXTRACT_GT  = 8
 };
 
 // Select data for extract
-int   CONF_extract = EXTRACT_MAP | EXTRACT_DBC | EXTRACT_GT;
+int   CONF_extract = EXTRACT_MAP | EXTRACT_DBC | EXTRACT_CAMERA | EXTRACT_GT;
 
 // This option allow limit minimum height to some value (Allow save some memory)
 bool  CONF_allow_height_limit = true;
@@ -177,7 +190,7 @@ void Usage(char const* prg)
         "%s -[var] [value]\n"\
         "-i set input path\n"\
         "-o set output path\n"\
-        "-e extract only MAP(1)/DBC(2)/gt(8) - standard: all(11)\n"\
+        "-e extract only MAP(1)/DBC(2)/Camera(4)/gt(8) - standard: all(11)\n"\
         "-f height stored as int (less map size but lost some accuracy) 1 by default\n"\
         "-l dbc locale\n"\
         "Example: %s -f 0 -i \"c:\\games\\game\"\n", prg, prg);
@@ -190,10 +203,10 @@ void HandleArgs(int argc, char* arg[])
     {
         // i - input path
         // o - output path
-        // e - extract only MAP(1)/DBC(2) - standard both(3)
+        // e - extract only MAP(1)/DBC(2)/Camera(4)/gt(8) - standard: all(11)
         // f - use float to int conversion
         // h - limit minimum height
-        // b - target client build
+        // l - dbc locale
         if (arg[c][0] != '-')
             Usage(arg[0]);
 
@@ -294,7 +307,7 @@ uint32 ReadBuild(int locale)
 
 void ReadMapDBC()
 {
-    printf("Read Map.dbc file... ");
+    printf("Read Map.db2 file...\n");
 
     CASC::FileHandle dbcFile = CASC::OpenFile(CascStorage, "DBFilesClient\\Map.db2", CASC_LOCALE_NONE, true);
     if (!dbcFile)
@@ -350,7 +363,7 @@ void ReadMapDBC()
 
 void ReadLiquidTypeTableDBC()
 {
-    printf("Read LiquidType.dbc file...");
+    printf("Read LiquidType.db2 file...\n");
     CASC::FileHandle dbcFile = CASC::OpenFile(CascStorage, "DBFilesClient\\LiquidType.db2", CASC_LOCALE_NONE, true);
     if (!dbcFile)
     {
@@ -381,6 +394,38 @@ void ReadLiquidTypeTableDBC()
         LiqType[db2.GetRowCopy(x).second] = LiqType[db2.GetRowCopy(x).first];
 
     printf("Done! (" SZFMTD " LiqTypes loaded)\n", LiqType.size());
+}
+
+bool ReadCinematicCameraDBC()
+{
+    printf("Read CinematicCamera.db2 file...\n");
+
+    CASC::FileHandle dbcFile = CASC::OpenFile(CascStorage, "DBFilesClient\\CinematicCamera.db2", CASC_LOCALE_NONE, true);
+    if (!dbcFile)
+    {
+        printf("Unable to open CinematicCamera.db2. Camera extract aborted.\n");
+        return false;
+    }
+
+    DB2FileLoader db2;
+    if (!db2.Load(dbcFile, CinematicCameraMeta::Instance()))
+    {
+        printf("Invalid CinematicCamera.db2 file format. Camera extract aborted. %s\n", HumanReadableCASCError(GetLastError()));
+        return false;
+    }
+
+    // get camera file list from DB2
+    for (size_t i = 0; i < db2.GetNumRows(); ++i)
+    {
+        std::string camFile(db2.getRecord(i).getString(0, 0));
+        size_t loc = camFile.find(".mdx");
+        if (loc != std::string::npos)
+            camFile.replace(loc, 4, ".m2");
+        CameraFileNames.insert(camFile);
+    }
+
+    printf("Done! (" SZFMTD " CinematicCameras loaded)\n", CameraFileNames.size());
+    return true;
 }
 
 //
@@ -1192,6 +1237,38 @@ void ExtractDBFilesClient(int l)
     printf("Extracted %u files\n\n", count);
 }
 
+void ExtractCameraFiles()
+{
+    printf("Extracting camera files...\n");
+
+    if (!ReadCinematicCameraDBC())
+        return;
+
+    boost::filesystem::path outputPath = output_path / "cameras";
+
+    CreateDir(outputPath);
+
+    printf("output path %s\n", outputPath.string().c_str());
+
+    // extract M2s
+    uint32 count = 0;
+    for (std::string const& cameraFileName : CameraFileNames)
+    {
+        if (CASC::FileHandle dbcFile = CASC::OpenFile(CascStorage, cameraFileName.c_str(), CASC_LOCALE_NONE))
+        {
+            boost::filesystem::path filePath = outputPath / GetCascFilenamePart(cameraFileName.c_str());
+
+            if (!boost::filesystem::exists(filePath))
+                if (ExtractFile(dbcFile, filePath.string()))
+                    ++count;
+        }
+        else
+            printf("Unable to open file %s in the archive: %s\n", cameraFileName.c_str(), HumanReadableCASCError(GetLastError()));
+    }
+
+    printf("Extracted %u camera files\n", count);
+}
+
 void ExtractGameTables()
 {
     printf("Extracting game tables...\n");
@@ -1341,6 +1418,13 @@ int main(int argc, char * arg[])
     {
         printf("No locales detected\n");
         return 0;
+    }
+
+    if (CONF_extract & EXTRACT_CAMERA)
+    {
+        OpenCascStorage(FirstLocale);
+        ExtractCameraFiles();
+        CascStorage.reset();
     }
 
     if (CONF_extract & EXTRACT_GT)
allankrdec/TrinityCore-10-2024

[3.3.5] Azjol-Nerub rewrite
* Scripts/AzjolNerub: Complete rewrite.
- Gatewatcher:
  - Trash now actually engages properly one by one
  - Fix trash spell casting
  - Add missing quotes
- Hadronox:
  - Everything. Literally.
- Anub'arak:
  - Fix add spawns
  - Fix impale
  - OK this might as well be "fix everything with every boss". Because that's pretty much what happened.
- General hack cleanup

* Update and rename 9999_99_99_99_AZJOLNERUB.sql to 2016_09_25_01_world.sql

* Update and rename 2016_09_25_01_world.sql to 2016_09_25_02_world.sql

* Rename 2016_09_25_02_world.sql to 2016_09_26_02_world.sql

src/server/scripts/Northrend/AzjolNerub/AzjolNerub/azjol_nerub.h
@@ -18,7 +18,6 @@
 #ifndef AZJOL_NERUB_H_
 #define AZJOL_NERUB_H_
 
-#define AzjolNerubScriptName "instance_azjol_nerub"
 #define DataHeader           "AN"
 
 uint32 const EncounterCount = 3;
@@ -26,14 +25,15 @@ uint32 const EncounterCount = 3;
 enum DataTypes
 {
     // Encounter States/Boss GUIDs
-    DATA_KRIKTHIR_THE_GATEWATCHER   = 0,
+    DATA_KRIKTHIR                   = 0,
     DATA_HADRONOX                   = 1,
     DATA_ANUBARAK                   = 2,
 
     // Additional Data
-    DATA_WATCHER_GASHRA             = 3,
-    DATA_WATCHER_SILTHIK            = 4,
-    DATA_WATCHER_NARJIL             = 5
+    DATA_WATCHER_NARJIL,
+    DATA_WATCHER_GASHRA,
+    DATA_WATCHER_SILTHIK,
+    DATA_ANUBARAK_WALL
 };
 
 enum CreatureIds
@@ -47,6 +47,12 @@ enum CreatureIds
     NPC_WATCHER_SILTHIK             = 28731
 };
 
+// These are passed as -action to AI's DoAction to differentiate between them and boss scripts' own actions
+enum InstanceActions
+{
+    ACTION_GATEWATCHER_GREET        = 1
+};
+
 enum GameObjectIds
 {
     GO_KRIKTHIR_DOOR                = 192395,
@@ -54,11 +60,5 @@ enum GameObjectIds
     GO_ANUBARAK_DOOR_2              = 192397,
     GO_ANUBARAK_DOOR_3              = 192398
 };
-
-template<class AI>
-AI* GetAzjolNerubAI(Creature* creature)
-{
-    return GetInstanceAI<AI>(creature, AzjolNerubScriptName);
-}
 
 #endif // AZJOL_NERUB_H_

src/server/scripts/Northrend/AzjolNerub/AzjolNerub/instance_azjol_nerub.cpp
@@ -21,7 +21,7 @@
 
 DoorData const doorData[] =
 {
-    { GO_KRIKTHIR_DOOR,     DATA_KRIKTHIR_THE_GATEWATCHER,  DOOR_TYPE_PASSAGE },
+    { GO_KRIKTHIR_DOOR,     DATA_KRIKTHIR,  DOOR_TYPE_PASSAGE },
     { GO_ANUBARAK_DOOR_1,   DATA_ANUBARAK,                  DOOR_TYPE_ROOM },
     { GO_ANUBARAK_DOOR_2,   DATA_ANUBARAK,                  DOOR_TYPE_ROOM },
     { GO_ANUBARAK_DOOR_3,   DATA_ANUBARAK,                  DOOR_TYPE_ROOM },
@@ -30,17 +30,32 @@ DoorData const doorData[] =
 
 ObjectData const creatureData[] =
 {
-    { NPC_KRIKTHIR,        DATA_KRIKTHIR_THE_GATEWATCHER },
+    { NPC_KRIKTHIR,        DATA_KRIKTHIR },
+    { NPC_HADRONOX,        DATA_HADRONOX                 },
+    { NPC_ANUBARAK,        DATA_ANUBARAK                 },
     { NPC_WATCHER_NARJIL,  DATA_WATCHER_GASHRA           },
     { NPC_WATCHER_GASHRA,  DATA_WATCHER_SILTHIK          },
     { NPC_WATCHER_SILTHIK, DATA_WATCHER_NARJIL           },
     { 0,                   0                             } // END
 };
 
+ObjectData const gameobjectData[] =
+{
+    { GO_ANUBARAK_DOOR_3, DATA_ANUBARAK_WALL },
+    { 0,                  0                  } // END
+};
+
+BossBoundaryData const boundaries = 
+{
+    { DATA_KRIKTHIR, new RectangleBoundary(400.0f, 580.0f, 623.5f, 810.0f) },
+    { DATA_HADRONOX, new ZRangeBoundary(666.0f, 776.0f) },
+    { DATA_ANUBARAK, new CircleBoundary(Position(550.6178f, 253.5917f), 26.0f) }
+};
+
 class instance_azjol_nerub : public InstanceMapScript
 {
     public:
-        instance_azjol_nerub() : InstanceMapScript(AzjolNerubScriptName, 601) { }
+        instance_azjol_nerub() : InstanceMapScript("instance_azjol_nerub", 601) { }
 
         struct instance_azjol_nerub_InstanceScript : public InstanceScript
         {
@@ -48,8 +63,19 @@ class instance_azjol_nerub : public InstanceMapScript
             {
                 SetHeaders(DataHeader);
                 SetBossNumber(EncounterCount);
+                LoadBossBoundaries(boundaries);
                 LoadDoorData(doorData);
-                LoadObjectData(creatureData, nullptr);
+                LoadObjectData(creatureData, gameobjectData);
+            }
+
+            void OnUnitDeath(Unit* who) override
+            {
+                InstanceScript::OnUnitDeath(who);
+                Creature* creature = who->ToCreature();
+                if (!creature || creature->IsCritter() || creature->IsControlledByPlayer())
+                    return;
+                if (Creature* gatewatcher = GetCreature(DATA_KRIKTHIR))
+                    gatewatcher->AI()->DoAction(-ACTION_GATEWATCHER_GREET);
             }
         };
 

Krigsgaldrnet/TrinityCore-Master

(cherry picked from commit 4b990eb7d7fda3951e640d84a5c18c90366cd26b)
# Conflicts:
#	src/server/game/Spells/SpellMgr.cpp
#	src/server/scripts/Northrend/AzjolNerub/AzjolNerub/azjol_nerub.h
#	src/server/scripts/Northrend/AzjolNerub/AzjolNerub/boss_anubarak.cpp
Update boss_hadronox.cpp
(cherry picked from commit 04ec868768d99c97f1cc4d064892bdeee2cbfb17)
I hate travis, learn from buildbot
(cherry picked from commit aeb20951bbac5453d5a4db22d23516ba8f948275)
I think I got all of them this time
No idea how this one got past travis.
(cherry picked from commit ae163197a765b294c0a325b6e893d216e1c5d050)
Fix nonPCH.
(cherry picked from commit e121d66fbb8edd68fcf027a05d47324757656228)
...for real this time (seriously - what happened to test builds before merging, Aok?)
(cherry picked from commit dbe0789eba03deb444f0f72e7e79e3be3763662a)
.....hail PCH
(cherry picked from commit 2528194cc1e481a6f002cc0663b12652e7e3de33)
and you get a header, and everyone gets a header...
(cherry picked from commit 5e5fe37a7ef2a76d7bd59250a7702af052a03d20)

src/server/scripts/Northrend/AzjolNerub/AzjolNerub/azjol_nerub.h
@@ -31,9 +31,10 @@ enum ANDataTypes
     DATA_ANUBARAK                   = 2,
 
     // Additional Data
-    DATA_WATCHER_GASHRA             = 3,
-    DATA_WATCHER_SILTHIK            = 4,
-    DATA_WATCHER_NARJIL             = 5
+    DATA_WATCHER_NARJIL,
+    DATA_WATCHER_GASHRA,
+    DATA_WATCHER_SILTHIK,
+    DATA_ANUBARAK_WALL
 };
 
 enum ANCreatureIds
@@ -53,6 +54,12 @@ enum ANGameObjectIds
     GO_ANUBARAK_DOOR_1              = 192396,
     GO_ANUBARAK_DOOR_2              = 192397,
     GO_ANUBARAK_DOOR_3              = 192398
+};
+
+// These are passed as -action to AI's DoAction to differentiate between them and boss scripts' own actions
+enum ANInstanceActions
+{
+    ACTION_GATEWATCHER_GREET        = 1
 };
 
 template<class AI>

src/server/scripts/Northrend/AzjolNerub/AzjolNerub/instance_azjol_nerub.cpp
@@ -31,10 +31,25 @@ DoorData const doorData[] =
 ObjectData const creatureData[] =
 {
     { NPC_KRIKTHIR,        DATA_KRIKTHIR_THE_GATEWATCHER },
+    { NPC_HADRONOX,        DATA_HADRONOX                 },
+    { NPC_ANUBARAK,        DATA_ANUBARAK                 },
     { NPC_WATCHER_NARJIL,  DATA_WATCHER_GASHRA           },
     { NPC_WATCHER_GASHRA,  DATA_WATCHER_SILTHIK          },
     { NPC_WATCHER_SILTHIK, DATA_WATCHER_NARJIL           },
     { 0,                   0                             } // END
+};
+
+ObjectData const gameobjectData[] =
+{
+    { GO_ANUBARAK_DOOR_3, DATA_ANUBARAK_WALL },
+    { 0,                  0                  } // END
+};
+
+BossBoundaryData const boundaries = 
+{
+    { DATA_KRIKTHIR_THE_GATEWATCHER, new RectangleBoundary(400.0f, 580.0f, 623.5f, 810.0f) },
+    { DATA_HADRONOX, new ZRangeBoundary(666.0f, 776.0f) },
+    { DATA_ANUBARAK, new CircleBoundary(Position(550.6178f, 253.5917f), 26.0f) }
 };
 
 class instance_azjol_nerub : public InstanceMapScript
@@ -48,8 +63,19 @@ class instance_azjol_nerub : public InstanceMapScript
             {
                 SetHeaders(DataHeader);
                 SetBossNumber(EncounterCount);
+                LoadBossBoundaries(boundaries);
                 LoadDoorData(doorData);
-                LoadObjectData(creatureData, nullptr);
+                LoadObjectData(creatureData, gameobjectData);
+            }
+
+            void OnUnitDeath(Unit* who) override
+            {
+                InstanceScript::OnUnitDeath(who);
+                Creature* creature = who->ToCreature();
+                if (!creature || creature->IsCritter() || creature->IsControlledByPlayer())
+                    return;
+                if (Creature* gatewatcher = GetCreature(DATA_KRIKTHIR_THE_GATEWATCHER))
+                    gatewatcher->AI()->DoAction(-ACTION_GATEWATCHER_GREET);
             }
         };
 
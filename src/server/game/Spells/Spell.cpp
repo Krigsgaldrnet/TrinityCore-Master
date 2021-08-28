TrinityCore/TrinityCore

Core/Spells: rework part 1: Improved positive detection logic

src/server/game/Spells/Spell.cpp
@@ -2671,7 +2671,8 @@ SpellMissInfo Spell::DoSpellHitOnUnit(Unit* unit, uint32 effectMask, bool scaleA
                 {
                     for (uint8 i = 0; i < MAX_SPELL_EFFECTS; ++i)
                     {
-                        if ((effectMask & (1 << i)) && !aurSpellInfo->IsPositiveEffect(i))
+                        // mod duration only for effects applying aura!
+                        if ((aura_effmask & (1 << i)) && !aurSpellInfo->IsPositiveEffect(i))
                         {
                             positive = false;
                             break;

Krigsgaldrnet/TrinityCore-Master

(cherry picked from commit efeae33495c8b57ae04aeeb382ee85099ac0b600)

src/server/game/Spells/Spell.cpp
@@ -2799,7 +2799,8 @@ SpellMissInfo Spell::DoSpellHitOnUnit(Unit* unit, uint32 effectMask)
                 {
                     for (uint8 i = 0; i < MAX_SPELL_EFFECTS; ++i)
                     {
-                        if ((effectMask & (1 << i)) && !m_spellInfo->IsPositiveEffect(i))
+                        // mod duration only for effects applying aura!
+                        if ((aura_effmask & (1 << i)) && !m_spellInfo->IsPositiveEffect(i))
                         {
                             positive = false;
                             break;
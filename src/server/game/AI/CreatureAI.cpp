allankrdec/TrinityCore-10-2024

Core/Creatures: Fixed crash in CreatureAI::JustAppeared caused by scripted summons without SummonProperties
Closes #23507

src/server/game/AI/CreatureAI.cpp
@@ -158,8 +158,9 @@ void CreatureAI::JustAppeared()
     if (!me->IsSummon())
         return;
 
+    // Summons without SummonProperties are generally scripted summons that don't belong to any owner
     TempSummon* summon = me->ToTempSummon();
-    if (summon->m_Properties->Category != SUMMON_CATEGORY_UNK && summon->m_Properties->Category != SUMMON_CATEGORY_PET)
+    if (!summon->m_Properties || (summon->m_Properties->Category != SUMMON_CATEGORY_UNK && summon->m_Properties->Category != SUMMON_CATEGORY_PET))
         return;
 
     // Not applied to vehicles

Krigsgaldrnet/TrinityCore-Master

(cherry picked from commit c0b25c9a83126bfd26da8245a31ea684008d804d)

src/server/game/AI/CreatureAI.cpp
@@ -165,8 +165,9 @@ void CreatureAI::JustAppeared()
     if (!me->IsSummon())
         return;
 
+    // Summons without SummonProperties are generally scripted summons that don't belong to any owner
     TempSummon* summon = me->ToTempSummon();
-    if (summon->m_Properties->Control != SUMMON_CATEGORY_UNK && summon->m_Properties->Control != SUMMON_CATEGORY_PET)
+    if (!summon->m_Properties || (summon->m_Properties->Control != SUMMON_CATEGORY_UNK && summon->m_Properties->Control != SUMMON_CATEGORY_PET))
         return;
 
     // Not applied to vehicles
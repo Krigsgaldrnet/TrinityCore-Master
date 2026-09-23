526f939bef9877a8d268280390e61e5674c04e45
Aqua Deus <95978183+aquadeus@users.noreply.github.com>
2026-09-24 00:19:11 +0200

    Core/Auras: Implement gravity opcodes and SPELL_AURA_MOD_GRAVITY (https://github.com/TrinityCore/TrinityCore/pull/31997)

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1193,6 +1193,8 @@ class TC_GAME_API Unit : public WorldObject
         void ApplyInertia(int32 id, Milliseconds duration);
         void RemoveInertia(int32 id);
 
+        void SetGravityModifier(float gravityModifier);
+
         void SetInFront(WorldObject const* target);
         void SetFacingTo(float const ori, bool force = true);
         void SetFacingToObject(WorldObject const* object, bool force = true);
